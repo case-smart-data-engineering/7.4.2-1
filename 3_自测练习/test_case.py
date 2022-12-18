06e9b951d7fb4a6a02dab1a9668082da1b1ae8ed
12liuhe <2462213910@qq.com>
2022-12-18 12:15:37 +0000

    add

--- 3_自测练习/test_case.py
+++ 3_自测练习/test_case.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python3
-
-from my_solution import solution
+import os.path as osp
+from torch_geometric.utils import negative_sampling
+from torch_geometric.datasets import Planetoid
+import torch_geometric.transforms as T
+from torch_geometric.utils import train_test_split_edges
+import torch
+from torch_geometric.nn import GCNConv
+import torch.nn.functional as F
+from sklearn.metrics import roc_auc_score, average_precision_score
+import pandas as pd
+from my_solution import Test,Net
 
 
 # 测试用例
 def test_solution():
-    assert solution(8) == 9  # 判断输出结果，预期increment(8)应该为9
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    dataset = 'cora'  # 获取数据集并进行处理
+    path = osp.join(osp.dirname(osp.realpath(__file__)))
+    dataset = Planetoid(path, dataset, transform=T.NormalizeFeatures())
+    data = dataset[0]
+    data.train_mask = data.val_mask = data.test_mask = data.y = None
+    data = train_test_split_edges(data)
+    dataset[0].edge_index.to(device)
+    data = data.to(device)
+    model = Net(dataset.num_features, 64).to(device)
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=0.01)
+    z = Test(data, model) 
+    print(z)
+if __name__ == "__main__":
+    test_solution()
